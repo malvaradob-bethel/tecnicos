40d3fd3b072c77d09c5ed9cad6629df79c90dda7
malvarado_bethel <malvarado@betheltv.tv>
2026-06-24 20:41:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  descarguen la versión nueva.
  * ════════════════════════════════════════════════════════════════ */
 
-const CACHE_VER = 'bethel-v8';
+const CACHE_VER = 'bethel-v9';
 const CACHE_NAME = 'registro-ops-' + CACHE_VER;
 
 // Archivos que se guardan para uso sin conexión
